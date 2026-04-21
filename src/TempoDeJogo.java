acc7f40c02b8399ccc5531f0381b0d922e04fd9c
Iago Lima <iagoclima22@gmail.com>
2026-04-21 09:57:16 -0300

    Problema tempo_de_jogo resolvido

---
+++ src/TempoDeJogo.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class TempoDeJogo {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int horaInicial, horaFinal, duracao;
+		
+		System.out.print("Hora inicial: ");
+		horaInicial = sc.nextInt();
+		System.out.print("Hora final: ");
+		horaFinal = sc.nextInt();
+		
+		if (horaInicial < horaFinal) {
+			duracao = horaFinal - horaInicial;
+		}
+		else {
+			duracao = 24 - horaInicial + horaFinal;
+		}
+		
+		System.out.println("O JOGO DUROU " + duracao + " HORA(S)");
+		
+		sc.close();
+	}
+}

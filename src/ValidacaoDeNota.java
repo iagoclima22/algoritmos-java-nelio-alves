c2fbfe8946ffeaea0357b24aa23445bfa88b6ed8
Iago Campanhol de Lima <iagoclima22@gmail.com>
2026-04-22 13:17:22 -0300

    Problema validacao_de_nota resolvido

---
+++ src/ValidacaoDeNota.java
@@ -0,0 +1,32 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ValidacaoDeNota {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double nota1, nota2;
+		
+		System.out.print("Digite a primeira nota: ");
+		nota1 = sc.nextDouble();
+		while (nota1 < 0 || nota1 > 10) {
+			System.out.print("Valor invalido! Tente novamente: ");
+			nota1 = sc.nextDouble();
+		}
+		
+		System.out.print("Digite a segunda nota: ");
+		nota2 = sc.nextDouble();
+		while (nota2 < 0 || nota2 > 10) {
+			System.out.print("Valor invalido! Tente novamente: ");
+			nota2 = sc.nextDouble();
+		}
+		
+		double media = (nota1 + nota2) / 2.0;
+		System.out.printf("MEDIA = %.2f", media);
+		
+		sc.close();
+	}
+}

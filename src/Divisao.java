1c88c9b4ed2d88a55cd906ed3a8e18e9093408dd
Iago Campanhol de Lima <iagoclima22@gmail.com>
2026-04-23 12:04:53 -0300

    Problema divisao resolvido

---
+++ src/Divisao.java
@@ -0,0 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Divisao {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Quantos casos voce vai digitar? ");
+		int n = sc.nextInt();
+		
+		int numerador, denominador;
+		double divisao;
+		for (int i=0; i<n; i++) {
+			System.out.print("Entre com o numerador: ");
+			numerador = sc.nextInt();
+			System.out.print("Entre com o denominador: ");
+			denominador = sc.nextInt();
+			if (denominador == 0) {
+				System.out.println("DIVISAO IMPOSSIVEL");
+			}
+			else {
+				divisao = (double) numerador / denominador;
+				System.out.printf("DIVISAO = %.2f%n", divisao);
+			}
+		}
+		
+		
+		sc.close();
+	}
+}

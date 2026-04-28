2a4f9ab6f7dafa6af4c78183ece4d39821baff28
Iago Lima <iagoclima22@gmail.com>
2026-04-27 23:19:06 -0300

    Problema soma_vetor resolvido

---
+++ src/SomaVetor.java
@@ -0,0 +1,39 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class SomaVetor {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Quantos numeros voce vai digitar? ");
+		int n = sc.nextInt();
+		
+		double[] vet = new double[n];
+		
+		for (int i=0; i<vet.length; i++) {
+			System.out.print("Digite um numero: ");
+			vet[i] = sc.nextDouble();
+		}
+		
+		System.out.println();
+		System.out.print("VALORES =");
+		for (int i=0; i<vet.length; i++) {
+			System.out.print(" " + vet[i]);
+		}
+		System.out.println();
+		
+		double soma = 0.0;
+		for (int i=0; i<vet.length; i++) {
+			soma += vet[i];
+		}
+		System.out.printf("SOMA = %.2f%n", soma);
+		double media = soma / n;
+		System.out.printf("MEDIA = %.2f", media);
+		
+		
+		sc.close();
+	}
+}

93a2c0cab12421bc03fec0bce0089b6b33bd5f4b
Iago Lima <iagoclima22@gmail.com>
2026-07-23 23:17:54 -0300

    Problema terreno resolvido

---
+++ src/estruturasequencial/Terreno.java
@@ -0,0 +1,24 @@
+package estruturasequencial;
+
+import java.util.Scanner;
+
+public class Terreno {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double largura, comprimento, metroQuadrado, area, preco;
+        System.out.print("Digite a largura do terreno: ");
+        largura = sc.nextDouble();
+        System.out.print("Digite o comprimento do terreno: ");
+        comprimento = sc.nextDouble();
+        System.out.print("Digite o valor do metro quadrado: ");
+        metroQuadrado = sc.nextDouble();
+
+        area = largura * comprimento;
+        preco = metroQuadrado * area;
+        System.out.printf("Area do terreno: %.2f%n", area);
+        System.out.printf("Preco do terreno: %.2f%n", preco);
+
+        sc.close();
+    }
+}

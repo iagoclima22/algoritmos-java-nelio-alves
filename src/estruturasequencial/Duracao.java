500a3bbaf15bc076efe20bf88d76bd13ba989ab5
Iago Lima <iagoclima22@gmail.com>
2026-07-26 01:11:55 -0300

    Problema duracao resolvido

---
+++ src/estruturasequencial/Duracao.java
@@ -0,0 +1,20 @@
+package estruturasequencial;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Duracao {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.print("Digite a duração em segundos: ");
+        int duracao = sc.nextInt();
+        int horas = duracao / 3600;
+        int minutos = duracao % 3600 / 60;
+        int segundos = duracao % 3600 % 60;
+        System.out.printf("%d:%d:%d", horas, minutos, segundos);
+
+        sc.close();
+    }
+}

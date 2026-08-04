85440f71f523e94b7bd2aa8c19853f8ee8908728
Iago Lima <iagoclima22@gmail.com>
2026-08-04 13:30:25 -0300

    Problema crescente resolvido

---
+++ src/Bestruturacondicional/Crescente.java
@@ -0,0 +1,27 @@
+package Bestruturacondicional;
+
+import java.util.Scanner;
+
+public class Crescente {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite dois números:");
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        while (x != y) {
+            if (x > y) {
+                System.out.println("DECRESCENTE!");
+            } else {
+                System.out.println("CRESCENTE!");
+            }
+
+            System.out.println("Digite outros dois números:");
+            x = sc.nextInt();
+            y = sc.nextInt();
+        }
+
+        sc.close();
+    }
+}

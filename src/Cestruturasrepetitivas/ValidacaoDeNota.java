6bd1145dd31c912496454d5771f965d2d810d0a3
Iago Lima <iagoclima22@gmail.com>
2026-08-05 11:11:07 -0300

    Problema validacao_de_nota

---
+++ src/Cestruturasrepetitivas/ValidacaoDeNota.java
@@ -0,0 +1,30 @@
+package Cestruturasrepetitivas;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ValidacaoDeNota {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite a primeira nota: ");
+        double n1 = sc.nextDouble();
+        while (n1 < 0 || n1 > 10) {
+            System.out.print("Valor inválido! Tente novamente: ");
+            n1 = sc.nextDouble();
+        }
+
+        System.out.print("Digite a segunda nota: ");
+        double n2 = sc.nextDouble();
+        while (n2 < 0 || n2 > 10) {
+            System.out.print("Valor inválido! Tente novamente: ");
+            n2 = sc.nextDouble();
+        }
+
+        double media = (n1 + n2) / 2;
+        System.out.printf("MEDIA = %.2f", media);
+
+        sc.close();
+    }
+}

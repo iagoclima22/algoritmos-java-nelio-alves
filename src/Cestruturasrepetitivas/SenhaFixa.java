89cb01bb7caeba2c0df1b3d4182f0a586c4f3a16
Iago Lima <iagoclima22@gmail.com>
2026-08-04 23:42:58 -0300

    Problema senha_fixa resolvido

---
+++ src/Cestruturasrepetitivas/SenhaFixa.java
@@ -0,0 +1,20 @@
+package Cestruturasrepetitivas;
+
+import java.util.Scanner;
+
+public class SenhaFixa {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int senhaCorreta = 2002;
+
+        System.out.print("Digite a senha: ");
+        int senha = sc.nextInt();
+
+        while (senha != senhaCorreta) {
+            System.out.print("Senha Inválida! Tente novamente: ");
+            senha = sc.nextInt();
+        }
+
+        System.out.println("Acesso permitido!");
+    }
+}
